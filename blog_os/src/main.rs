6e91ebc2563a3756aa14b730d427eba57e0e917d
jwz <3544346335@qq.com>
2023-12-22 19:09:07 -0500

    易失操作前的写入字符

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -1,7 +1,7 @@
 #![no_std]//不链接Rust标准库
 #![no_main]//禁用所有Rust层级的入口点
 use core::panic::PanicInfo;
-
+mod vga_buffer;
 //这个函数将在panic时被调用
 #[panic_handler]
 fn panic(info:&PanicInfo)->!{
@@ -11,15 +11,16 @@ static HELLO:&[u8] = b"Hello World!";
 
 #[no_mangle]//不重整函数名
 pub extern "C" fn _start()->!{
-    let vga_buffer = 0xb8000 as *mut u8;
+    // let vga_buffer = 0xb8000 as *mut u8;
 
-    for(i,&byte) in HELLO.iter().enumerate(){
-        unsafe{
-            *vga_buffer.offset(i as isize*2) = byte;
-            *vga_buffer.offset(i as isize*2+1) = 0xb;
-        }
-    }
+    // for(i,&byte) in HELLO.iter().enumerate(){
+    //     unsafe{
+    //         *vga_buffer.offset(i as isize*2) = byte;
+    //         *vga_buffer.offset(i as isize*2+1) = 0xb;
+    //     }
+    // }
 
+    vga_buffer::print_something();
     loop{}
 }
     
--- blog_os/target/x86_64-blog_os/debug/deps/blog_os-60c63be87eae6ce5.d
+++ blog_os/target/x86_64-blog_os/debug/deps/blog_os-60c63be87eae6ce5.d
@@ -1,5 +1,6 @@
-/home/jwz/ZcoreOS/blog_os/target/x86_64-blog_os/debug/deps/blog_os-60c63be87eae6ce5: src/main.rs
+/home/jwz/ZcoreOS/blog_os/target/x86_64-blog_os/debug/deps/blog_os-60c63be87eae6ce5: src/main.rs src/vga_buffer.rs
 
-/home/jwz/ZcoreOS/blog_os/target/x86_64-blog_os/debug/deps/blog_os-60c63be87eae6ce5.d: src/main.rs
+/home/jwz/ZcoreOS/blog_os/target/x86_64-blog_os/debug/deps/blog_os-60c63be87eae6ce5.d: src/main.rs src/vga_buffer.rs
 
 src/main.rs:
+src/vga_buffer.rs:
